59afbf43de6e118d2db13935c65f294409227044
Kerautret <bertrand.kerautret@univ-lorraine.fr>
2016-03-24 13:15:07 +0100

    fix with current DGtal >= 0.9.2

--- DGtalBranchVisu/main.cpp
+++ DGtalBranchVisu/main.cpp
@@ -1,4 +1,3 @@
-#include <qapplication.h>
 #include <DGtal/base/Common.h>
 #include <DGtal/io/viewers/Viewer3D.h>
 #include "DGtal/io/readers/VolReader.h"
@@ -11,6 +10,7 @@
 #include "DGtal/topology/helpers/Surfaces.h"
 #include "DGtal/images/imagesSetsUtils/SetFromImage.h"
 
+#include "DGtal/io/readers/GenericReader.h"
 
 
 #include "DGtal/io/readers/MeshReader.h"
@@ -22,22 +22,54 @@ using namespace Z3i;
 
 namespace po = boost::program_options;
 
+RealPoint
+getBarycenter(const KSpace &K,  const vector<SCell> &aScellVect){
+  RealPoint pointResu;
+  unsigned int nb=0;
+  for(vector<SCell>::const_iterator it = aScellVect.begin(); it!= aScellVect.end(); it++){
+    pointResu[0] += K.sCoord( *it, 0 );
+    pointResu[1] += K.sCoord( *it, 1 );
+    pointResu[2] += K.sCoord( *it, 2 );
+    nb++;
+  }
+  pointResu[0] /=nb;
+  pointResu[1] /=nb;
+  pointResu[2] /=nb;
+  return pointResu;
+}
+
 
 
 int main(int argc, char** argv)
 {
 
+
+  typedef DGtal::ImageContainerBySTLVector<DGtal::Z2i::Domain,  unsigned char > Image2D;
+  typedef DGtal::ImageContainerBySTLVector<DGtal::Z3i::Domain,  unsigned char > Image3D;
+  typedef DGtal::ConstImageAdapter<Image3D, Image2D::Domain, DGtal::functors::Projector< DGtal::Z3i::Space>,
+				   Image3D::Value,  DGtal::functors::Identity >  SliceImageAdapter;
+
+  typedef DGtal::Viewer3D<Z3i::Space, Z3i::KSpace> My3DViewer;  
   // parse command line ----------------------------------------------
   po::options_description general_opt("Allowed options are: ");
   general_opt.add_options()
 	("help,h", "display this message")
 	("input-file,i", po::value<std::string>(), "vol file (.vol) , pgm3d (.p3d or .pgm3d) file or sdp (sequence of discrete points)" )
-	("trunkBark-mesh,t", po::value<std::string>(), "mesh of the trunk bark in format OFS non normalized (.ofs)" )
+	("volImageName,s", po::value<std::string>(), "specify the 3D image name to be used to display slice image (.vol, pgm3D, ...)." )
+    ("sliceZ",po::value<unsigned int>(), "add a SliceZ image of number <arg>" )
+    ("sliceY",po::value<unsigned int>(), "add a SliceY image of number <arg>" )
+    ("sliceX",po::value<unsigned int>(), "add a SliceX image of number <arg>" )
+	
+    ("trunkBark-mesh,t", po::value<std::string>(), "mesh of the trunk bark in format OFS non normalized (.ofs)" )
 	("marrow-mesh,a", po::value<std::string>(), "mesh of trunk marrow  in format OFS non normalized (.ofs)" )
-    	("scaleX,x",  po::value<float>()->default_value(1.0), "set the scale value in the X direction (default 1.0)" )
+    ("scaleX,x",  po::value<float>()->default_value(1.0), "set the scale value in the X direction (default 1.0)" )
 	("scaleY,y",  po::value<float>()->default_value(1.0), "set the scale value in the Y direction (default 1.0)" )
 	("scaleZ,z",  po::value<float>()->default_value(1.0), "set the scale value in the Z direction (default 1.0)")
-	("transparency,T",  po::value<uint>()->default_value(100), "transparency") ;
+    ("filterMinSize,m",  po::value<int>(), "filter according min size of connected component. The CC is given only of its size is bigger or eq than [arg]  " )
+("filterMaxX",  po::value<int>(), "filter according maximal X coordinate of the barycenter connected component. The CC is given only of its barycenter is less or eq than [arg]  " )
+    ("filterMaxY",  po::value<int>(), "filter according maximal Y coordinate of the barycenter connected component. The CC is given only of its barycenter is less or eqthan [arg]  " )
+    ("filterMaxZ",  po::value<int>(), "filter according maximal Z coordinate of the barycenter connected component. The CC is given only of its barycenter is less or eq than [arg]  " )
+    ("transparency,T",  po::value<uint>()->default_value(100), "transparency") ;
   bool parseOK=true;
   po::variables_map vm;
   try{
@@ -72,12 +104,59 @@ int main(int argc, char** argv)
   float sz = vm["scaleZ"].as<float>();
 
   QApplication application(argc,argv);
-  Viewer3D viewer;
+  My3DViewer viewer;
 
-  viewer.setScale(sx,sy,sz);
+  viewer.setGLScale(sx,sy,sz);
   viewer.setWindowTitle("simple Volume Viewer");
   viewer.show();
 
+  const functors::Identity identityFunctor{};
+  
+  if(vm.count("volImageName") &&(vm.count("sliceX")||vm.count("sliceY")||vm.count("sliceZ"))){
+    string imageName = vm["volImageName"].as<std::string>();
+    Image3D img = GenericReader<Image3D>::import(imageName);
+    
+    int nbImage=0;
+    if(vm.count("sliceX")){
+      unsigned int sliceNum = vm["sliceX"].as<unsigned int>();
+      DGtal::functors::Projector<DGtal::Z2i::Space>  invFunctorX; invFunctorX.initRemoveOneDim(0);
+      DGtal::Z2i::Domain domain2DX(invFunctorX(img.domain().lowerBound()), 
+				   invFunctorX(img.domain().upperBound()));
+      DGtal::functors::Projector<DGtal::Z3i::Space> aSliceFunctorX(sliceNum); aSliceFunctorX.initAddOneDim(0);
+      SliceImageAdapter sliceImageX(img, domain2DX, aSliceFunctorX, identityFunctor);
+      viewer << sliceImageX;
+      viewer << DGtal::UpdateImagePosition<Z3i::Space, Z3i::KSpace>(nbImage, My3DViewer::xDirection, sliceNum, 0.0,0.0 );
+      nbImage++;
+    }
+    
+    if(vm.count("sliceZ")){
+      unsigned int sliceNum = vm["sliceZ"].as<unsigned int>();
+      DGtal::functors::Projector<DGtal::Z2i::Space>  invFunctorZ; invFunctorZ.initRemoveOneDim(2);
+      DGtal::Z2i::Domain domain2DZ(invFunctorZ(img.domain().lowerBound()), 
+				   invFunctorZ(img.domain().upperBound()));
+      DGtal::functors::Projector<DGtal::Z3i::Space> aSliceFunctorZ(sliceNum); aSliceFunctorZ.initAddOneDim(2);
+      
+      SliceImageAdapter sliceImageZ(img, domain2DZ, aSliceFunctorZ, identityFunctor);
+      viewer << sliceImageZ;
+      viewer << DGtal::UpdateImagePosition<Z3i::Space, Z3i::KSpace>(nbImage, My3DViewer::zDirection,  0.0,0.0, sliceNum );
+      nbImage++;
+    }
+
+    if(vm.count("sliceY")){
+      unsigned int sliceNum = vm["sliceY"].as<unsigned int>();
+      DGtal::functors::Projector<DGtal::Z2i::Space>  invFunctorY; invFunctorY.initRemoveOneDim(1);
+      DGtal::Z2i::Domain domain2DY(invFunctorY(img.domain().lowerBound()), 
+				   invFunctorY(img.domain().upperBound()));
+
+      DGtal::functors::Projector<DGtal::Z3i::Space> aSliceFunctorY(sliceNum); aSliceFunctorY.initAddOneDim(1);
+      SliceImageAdapter sliceImageY(img, domain2DY, aSliceFunctorY, identityFunctor);
+      viewer << sliceImageY;
+      viewer << DGtal::UpdateImagePosition<Z3i::Space, Z3i::KSpace>(nbImage, My3DViewer::yDirection, 0.0, sliceNum,0.0 );
+      nbImage++;
+      
+    }
+    
+  }
 
 
   // Point for the domain
@@ -130,7 +209,7 @@ int main(int argc, char** argv)
 
 
 
-	GradientColorMap<long> gradient( 0,vectConnectedSCell.size());
+	GradientColorMap<long> gradient( 0,6);
 	gradient.addColor(DGtal::Color::Red);
 	gradient.addColor(DGtal::Color::Yellow);
 	gradient.addColor(DGtal::Color::Green);
@@ -138,27 +217,46 @@ int main(int argc, char** argv)
 	gradient.addColor(DGtal::Color::Blue);
 	gradient.addColor(DGtal::Color::Magenta);
 	gradient.addColor(DGtal::Color::Red);
-
+	unsigned int cptComp=0;
 	viewer << SetMode3D(vectConnectedSCell.at(0).at(0).className(), "Basic");
 	for(uint i=0; i< vectConnectedSCell.size();i++){
-	  DGtal::Color c= gradient(i);
-	  viewer << CustomColors3D(Color(250, 0,0,transp), Color(c.red(),
-							  c.green(),
-							  c.blue(),120));
-
-	  for(uint j=0; j< vectConnectedSCell.at(i).size();j++){
-	viewer << vectConnectedSCell.at(i).at(j);
+	  DGtal::Color c= gradient(cptComp%7);
+	  bool display=true;
+	  if(vm.count("filterMinSize")){
+	    unsigned int minSize = vm["filterMinSize"].as<int>();
+	    display= display && (vectConnectedSCell.at(i).size()>=minSize);
+	  }
+	  if(vm.count("filterMaxZ") || vm.count("filterMaxY") || vm.count("filterMaxX")){
+	    RealPoint barycenter =  getBarycenter(K,vectConnectedSCell.at(i)); 	    
+	    if(vm.count("filterMaxZ")){
+	      int maxZ = vm["filterMaxZ"].as<int>();
+	      display=display && (barycenter[2]<= maxZ);
+	    }
+	    if(vm.count("filterMaxY")){
+	      int maxY = vm["filterMaxY"].as<int>();
+	      display=display && (barycenter[1]<= maxY);
+	    }
+	    if(vm.count("filterMaxX")){
+	      int maxY = vm["filterMaxX"].as<int>();
+	      display=display && (barycenter[0]<= maxY);
+	    }
+	    
+	  }
+	  if(display){
+	    cptComp++;
+	    viewer << CustomColors3D(Color(250, 0, 0, transp), Color(c.red(),
+								     c.green(),
+								     c.blue(),transp));
+	    for(uint j=0; j< vectConnectedSCell.at(i).size();j++){
+	      viewer << vectConnectedSCell.at(i).at(j);
+	    }
 	  }
 	}
-
-
-
-
   }
 
   if(vm.count("trunkBark-mesh")){
 	string meshFilename = vm["trunkBark-mesh"].as<std::string>();
-	Mesh<Display3D::pointD3D> anImportedMesh(DGtal::Color(160, 30, 30,20));
+	DGtal::Mesh<Z3i::RealPoint> anImportedMesh(DGtal::Color(160, 30, 30,20));
 	anImportedMesh.invertVertexFaceOrder();
 	bool import = anImportedMesh << meshFilename;
 	if(import){
@@ -169,14 +267,17 @@ int main(int argc, char** argv)
 
   if(vm.count("marrow-mesh")){
 	string meshFilename = vm["marrow-mesh"].as<std::string>();
-	Mesh<Display3D::pointD3D> anImportedMesh(DGtal::Color(70,70,70,255));
+	Mesh<Z3i::RealPoint> anImportedMesh(DGtal::Color(70,70,70,255));
 	bool import = anImportedMesh << meshFilename;
 	if(import){
 	  viewer << anImportedMesh;
 	}
   }
 
-  viewer << Viewer3D::updateDisplay;
+
+
+
+  viewer << My3DViewer::updateDisplay;
 
 
 
